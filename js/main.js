4bd560a71ab8e711dd06cf9e9ef9115972c1bdaa
Artak Chatilyan <artak.chatilyan@gmail.com>
2023-02-16 20:56:12 +0400

    Define Blog block

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible-expanded");
   })
 );
+
+const ele = document.getElementsByClassName('blog__body')[0];
+ele.scrollLeft=160;
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+const mouseDownHandler = function (e) {
+  ele.style.cursor = 'grabbing';
+  ele.style.userSelect = 'none';
+    pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        x: e.clientX,
+        y: e.clientY,
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+  const dx = e.clientX - pos.x;
+  const dy = e.clientY - pos.y;
+
+  ele.scrollTop = pos.top - dy;
+  ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+  document.removeEventListener('mousemove', mouseMoveHandler);
+  document.removeEventListener('mouseup', mouseUpHandler);
+
+  ele.style.cursor = 'grab';
+  ele.style.removeProperty('user-select');
+};
+
+ele.addEventListener('mousedown', mouseDownHandler);
